52f2746d755adea87396c1c48132e4f76160fd00
ECJTU_nlpers <nlp_ecjtu@163.com>
2022-10-10 15:30:56 +0800

    Add files via upload

---
+++ P2 Text classification/SentimentUP/HAN_UP/model.py
@@ -0,0 +1,164 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import LayerNorm
+
+
+class EmbedLayer(nn.Module):
+    def __init__(self, we_tensor, finetune=False):
+        super(EmbedLayer, self).__init__()
+        self.embed = nn.Embedding(we_tensor.shape[0], we_tensor.shape[1])
+        self.embed.weight.data.copy_(we_tensor)
+        self.embed.weight.requires_grad = finetune
+
+    def forward(self, x):
+        return self.embed(x)
+
+
+
+class ProdAttnLayer(nn.Module):
+    def __init__(self, inpt_dim, p_dim):
+        super(ProdAttnLayer, self).__init__()
+        self.attn_inpt = nn.Linear(inpt_dim, inpt_dim, bias=True)
+        self.attn_p = nn.Linear(p_dim, inpt_dim, bias=False)
+        self.contx = nn.Linear(inpt_dim, 1, bias=False)
+
+    def forward(self, inpt, p, inpt_lens=None):
+        inpt_p = torch.tanh_(self.attn_inpt(inpt) + self.attn_p(p))
+        a = self.contx(inpt_p)
+        # w/o mask
+        # if inpt_lens is not None:
+        #     m = inpt_lens[:, :, None]
+        #     a = a - (1 - m) * 1e31
+        a = F.softmax(a, dim=1)
+        s = (a * inpt).sum(1)
+
+        return s, a.transpose(1, 2)
+
+
+
+class UserTranLayer(nn.Module):
+    def __init__(self, inpt_dim, u_dim, dropout=0.0):
+        super(UserTranLayer, self).__init__()
+        self.tran_linear1 = nn.Linear(inpt_dim + u_dim, inpt_dim)
+        self.tran_linear2 = nn.Linear(inpt_dim, inpt_dim)
+        self.drop = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+
+    def forward(self, inpt, u):
+        h = self.drop(self.relu(self.tran_linear1(torch.cat([inpt, u], dim=-1))) + inpt)
+        h = self.drop(self.relu(self.tran_linear2(h)) + h)
+
+        return h
+
+
+
+class HAN_UP(nn.Module):
+    def __init__(self, conf, we):
+        super(HAN_UP, self).__init__()
+        self.conf = conf
+
+        # random initialize user and prod as uniform distribution (-0.01, 0.01)
+        self.user_embed = nn.Embedding(conf.user_num, conf.user_dim)
+        nn.init.uniform_(self.user_embed.weight, -0.01, 0.01)
+
+        self.prod_embed = nn.Embedding(conf.prod_num, conf.prod_dim)
+        nn.init.uniform_(self.prod_embed.weight, -0.01, 0.01)
+        
+        # word embeddings
+        self.embed_layer = EmbedLayer(we, conf.finetune)
+
+        # word level
+        self.word_trans = UserTranLayer(conf.word_dim, conf.user_dim, conf.dropout)
+        self.word_norm = LayerNorm(conf.word_dim)
+        self.word_encoder = nn.LSTM(conf.word_dim, 
+                                    conf.word_hidden_size, 
+                                    bidirectional=True, 
+                                    batch_first=True)
+        self.word_attn = ProdAttnLayer(conf.word_hidden_size * 2, conf.prod_dim)
+
+        # sentence level
+        self.sent_trans = UserTranLayer(conf.word_hidden_size * 2, conf.user_dim, conf.dropout)
+        self.sent_norm = LayerNorm(conf.word_hidden_size * 2)
+        self.sent_encoder = nn.LSTM(conf.word_hidden_size*2,
+                                    conf.sent_hidden_size,
+                                    bidirectional=True,
+                                    batch_first=True)
+        self.sent_attn = ProdAttnLayer(conf.sent_hidden_size * 2, conf.prod_dim)
+
+        # document level
+        self.doc_trans = UserTranLayer(conf.sent_hidden_size * 2, conf.user_dim, conf.dropout)
+        self.doc_norm = LayerNorm(conf.sent_hidden_size * 2)
+
+        # mlp for claasification
+        self.mlp = nn.Linear(conf.sent_hidden_size * 2, conf.num_classes)
+        self.drop = nn.Dropout(conf.dropout)
+
+
+    def forward(self, u, p, x, sen_lens, doc_lens):
+        conf = self.conf
+
+        # load user and prodct embeddings
+        u_embed = self.user_embed(u)
+        p_embed = self.prod_embed(p)
+        
+        # load word embeddings
+        x_embed = self.embed_layer(x)
+        
+        # word -> sentence
+        # for shape adaptation
+        zeros_u = torch.zeros(x_embed.size(0), x_embed.size(1), x_embed.size(2), conf.user_dim)
+        zeros_u = zeros_u.to(self.conf.device)
+        zeros_p = torch.zeros(x_embed.size(0), x_embed.size(1), x_embed.size(2), conf.prod_dim)
+        zeros_p = zeros_p.to(self.conf.device)
+
+        u_embed_ = zeros_u + u_embed[:, None, None, :]
+        u_embed_ = u_embed_.view(-1, u_embed_.size(2), u_embed_.size(3))
+        x_embed = x_embed.view(-1, x_embed.size(2), x_embed.size(3))
+        
+        # transition word embeddings conditioning on user, and normalize them
+        x_embed = self.word_trans(x_embed, u_embed_)
+        x_embed = self.word_norm(x_embed)
+        
+        # learn word-level context based on BiLSTM
+        word_rep, _ = self.word_encoder(x_embed)
+
+        # shape adaptation
+        p_embed_ = zeros_p + p_embed[:, None, None, :]
+        p_embed_ = p_embed_.view(-1, p_embed_.size(2), p_embed_.size(3))
+        
+        # product-biased attention mechanism
+        sen_rep,  _ = self.word_attn(word_rep, p_embed_, sen_lens)
+        sen_rep     = sen_rep.view([-1, self.conf.max_sent_num, sen_rep.size(1)])
+        sen_rep     = self.drop(sen_rep)
+
+        # sentence -> document
+        # for shape adaptation
+        zeros_u = torch.zeros(sen_rep.size(0), sen_rep.size(1), conf.user_dim)
+        zeros_u = zeros_u.to(self.conf.device)
+        zeros_p = torch.zeros(sen_rep.size(0), sen_rep.size(1), conf.prod_dim)
+        zeros_p = zeros_p.to(self.conf.device)
+
+        # transition sentence representaions conditioning on user
+        # and normalize them
+        u_embed_ = zeros_u + u_embed[:, None, :]
+        sen_rep = self.sent_trans(sen_rep, u_embed_)
+        sen_rep = self.sent_norm(sen_rep)
+
+        # learn sentence-level context based on BiLSTM
+        sen_rep, _ = self.sent_encoder(sen_rep)
+
+        # product-biased attention mechanism
+        p_embed_ = zeros_p + p_embed[:, None, :]
+        doc_rep, _ = self.sent_attn(sen_rep, p_embed_, doc_lens)
+
+        # transition sentence representaions conditioning on user
+        doc_rep = self.doc_trans(doc_rep, u_embed)
+        # and normalize them, needed?
+        # doc_rep = self.doc_norm(doc_rep)
+        doc_rep = self.drop(doc_rep)
+
+        # classification
+        logits = self.mlp(doc_rep)
+
+        return logits
---
+++ P2 Text classification/SentimentUP/HAN_UP/train.py
@@ -0,0 +1,184 @@
+import os, sys
+import time
+import torch
+import argparse
+import utils
+
+import torch.nn as nn
+import numpy as np
+from model import HAN_UP
+from config import Config
+
+
+def eval(model, data):
+    model.eval()
+
+    data_size = data[0].shape[0]
+    acc = 0.
+    rmse = 0.
+
+    with torch.no_grad():
+        for batch in utils.batch_iter(data, conf.batch_size, True):
+            u, p, x, y, sen_lens, doc_lens = batch
+            u = torch.LongTensor(u).to(conf.device)
+            p = torch.LongTensor(p).to(conf.device)
+            x = torch.LongTensor(x).to(conf.device)
+            y = torch.LongTensor(y).to(conf.device)
+            sen_lens = torch.LongTensor(sen_lens)
+            doc_lens = torch.LongTensor(doc_lens)
+            sen_lens = sen_lens.view(-1)
+            sen_lens = utils.get_mask(sen_lens, conf.max_word_num)
+            doc_lens = utils.get_mask(doc_lens, conf.max_sent_num)
+            sen_lens = sen_lens.to(conf.device)
+            doc_lens = doc_lens.to(conf.device)
+
+            predicts = model(u, p, x, sen_lens, doc_lens)
+            predicts = predicts.max(1)[1]
+            predicts = predicts.cpu().detach().numpy()
+            y = y.cpu().detach().numpy()
+            acc += np.sum((predicts == y))
+            rmse += np.sum((predicts - y) ** 2)
+
+    acc = acc / data_size
+    rmse = np.sqrt(rmse/data_size)
+    return acc, rmse
+
+
+def train(conf, train_data, dev_data, optimizer, criterion):
+    train_size = train_data[0].shape[0]
+    eval_at = conf.eval_at * train_size
+    conf.eval_at = eval_at
+    stop_at = conf.stop_at
+    best_dev_acc = 0
+    best_dev_rmse = 100
+
+    start_time = time.time()
+    for epoch in range(conf.epochs):
+        if stop_at <= 0:
+            break
+
+        batch_i = 0
+        for batch in utils.batch_iter(train_data, conf.batch_size, True):
+            model.train()
+            u, p, x, y, sen_lens, doc_lens = batch
+            u = torch.LongTensor(u).to(conf.device)
+            p = torch.LongTensor(p).to(conf.device)
+            x = torch.LongTensor(x).to(conf.device)
+            y = torch.LongTensor(y).to(conf.device)
+            sen_lens = torch.LongTensor(sen_lens)
+            doc_lens = torch.LongTensor(doc_lens)
+            sen_lens = sen_lens.view(-1)
+            sen_lens = utils.get_mask(sen_lens, conf.max_word_num)
+            doc_lens = utils.get_mask(doc_lens, conf.max_sent_num)
+            sen_lens = sen_lens.to(conf.device)
+            doc_lens = doc_lens.to(conf.device)
+
+            predicts = model(u, p, x, sen_lens, doc_lens)
+            loss = criterion(predicts, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            batch_i += 1
+
+            eval_at -= x.size(0)
+            if eval_at <= 0:
+                improved = ''
+                dev_acc, dev_rmse = eval(model, dev_data)
+                
+                if best_dev_acc <= dev_acc:
+                    torch.save(model.state_dict(), conf.model)
+                    best_dev_acc = dev_acc
+                    best_dev_rmse = dev_rmse
+                    stop_at = conf.stop_at
+                    improved = '*'
+                else:
+                    stop_at -= 1
+                
+                end_time = time.time()
+                logg.info("")
+                logg.info("Epoch: %3d, Batch: %5d, Train Loss: %.4f, Dev Acc: %.2f, Dev RMSE: %.3f, Time: %.2f %s" % (
+                    epoch, batch_i, loss, dev_acc * 100, dev_rmse, (end_time-start_time), improved))
+
+                start_time = time.time()
+                eval_at = conf.eval_at
+
+    logg.info("Best dev acc: %.3f, Best dev rmse: %.3f" % (best_dev_acc, best_dev_rmse))
+    return
+
+
+if __name__ == '__main__':
+    os.chdir(sys.path[0])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=int, default=0)
+    parser.add_argument('--config', type=str, choices=['config/yelp2013.json', 'config/yelp2014.json', 'config/imdb.json'],
+                        default='config/yelp2013.json')
+
+    parser.add_argument('--dataset', type=str)
+    parser.add_argument('--dtrain', type=str)
+    parser.add_argument('--ddev', type=str)
+    parser.add_argument('--dtest', type=str)
+    
+    parser.add_argument('--outdir', type=str)
+    parser.add_argument('--logdir', type=str)
+
+    parser.add_argument('--user_dim', type=int)
+    parser.add_argument('--prod_dim', type=int)
+    
+    parser.add_argument('--word_embed', type=str)
+    parser.add_argument('--word_dim', type=int)
+    parser.add_argument('--finetune', type=int)
+    parser.add_argument('--word_hidden_size', type=int)
+    parser.add_argument('--sent_hidden_size', type=int)
+    
+    parser.add_argument('--epochs', type=int)
+    parser.add_argument('--batch_size', type=int)
+    parser.add_argument('--lr', type=float)
+    parser.add_argument('--dropout', type=float)
+    parser.add_argument('--stop_at', type=int)
+    parser.add_argument('--eval_at', type=int)
+    
+    # if no seed is specified, a random seed is used
+    parser.add_argument('--seed', type=int)
+    
+
+    args = parser.parse_args()
+    conf = Config(args)
+    
+    logg = utils.get_logger(conf.logdir)
+    conf.logg = logg
+    logg.info(conf)
+
+    utils.set_seed(conf.seed)
+    conf.device = torch.device('cuda:{0}'.format(conf.device) if torch.cuda.is_available() else 'cpu')
+    
+    # create user and product dictionary
+    user_dict, prod_dict = utils.load_up_dict(conf)
+    conf.user_num = len(user_dict)
+    conf.prod_num = len(prod_dict)
+    
+    # create word vocab
+    logg.info('Loading word embeddings')
+    vocab, embeddings = utils.load_embedings(conf)
+    embeddings = torch.Tensor(embeddings)
+    
+    # load training, dev and test data
+    logg.info('Loading training data')
+    train_data = utils.load_data(conf, vocab, user_dict, prod_dict, 'train')
+    logg.info('Loading dev data')
+    dev_data = utils.load_data(conf, vocab, user_dict, prod_dict, 'dev')
+    logg.info('Loading test data')
+    test_data = utils.load_data(conf, vocab, user_dict, prod_dict, 'test')
+    
+    # build and train model
+    model = HAN_UP(conf, embeddings)
+    model.to(conf.device)
+    optimizer = torch.optim.Adam(model.parameters(), conf.lr)
+    criterion = nn.CrossEntropyLoss() 
+    train(conf, train_data, dev_data, optimizer, criterion)
+
+    # load the model with best dev acc, then eval it on the test dataset 
+    model.load_state_dict(torch.load(conf.model))
+    test_acc, test_rmse = eval(model, test_data)
+    logg.info("Test Acc: %.2f, Test RMSE: %.3f" % (test_acc * 100, test_rmse))
+    logg.info("")
