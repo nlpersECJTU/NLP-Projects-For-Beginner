9d7daaed9a10501dbbd99e8fd1d221a9a2029bdf
ECJTU_nlpers <nlp_ecjtu@163.com>
2022-11-29 11:10:30 +0800

    Add files via upload

--- P5 Seq2Seq/mt/run.sh
+++ P5 Seq2Seq/mt/run.sh
@@ -33,3 +33,32 @@ sleep 5m
 
 python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 256 --use_beam 1 --less_gpu 1 --seed 12345
 sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 64 --use_beam 0 --seed 123
+sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 64 --use_beam 0 --seed 1234
+sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 64 --use_beam 0 --seed 12345
+sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 64 --use_beam 1 --less_gpu 1 --seed 123
+sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 64 --use_beam 1 --less_gpu 1 --seed 1234
+sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 64 --use_beam 1 --less_gpu 1 --seed 12345
+sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 256 --use_beam 1 --less_gpu 1 --beam_size 3 --seed 123
+sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 256 --use_beam 1 --less_gpu 1 --beam_size 4 --seed 1234
+sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 256 --use_beam 1 --less_gpu 1 --beam_size 5 --seed 12345
+sleep 5m
+
+python3 ~/nlptasks/mt/train.py --config config/multi30k_en_de.json --batch_size 256 --use_beam 1 --less_gpu 1 --beam_size 6 --seed 12345
